932b8b499127d91bd0bcab22ec34aa2e6c85732f
Alessandro Orciari <92159043+AleOrcia@users.noreply.github.com>
2024-10-14 13:59:03 +0200

    Update processes.py
    refactored and added gazebo bridge

--- px4_offboard/px4_offboard/processes.py
+++ px4_offboard/px4_offboard/processes.py
@@ -1,7 +1,5 @@
-
 #!/usr/bin/env python3
 
-# Import the subprocess and time modules
 import subprocess
 import time
 
@@ -11,16 +9,23 @@ commands = [
     "MicroXRCEAgent udp4 -p 8888",
 
     # Run the PX4 SITL simulation
-    "cd ~/PX4-Autopilot && PX4_GZ_STANDALONE=1 make px4_sitl gz_x500",
+    "cd ~/PX4-Autopilot && HEADLESS=1 make px4_sitl gz_x500",
 
     # Run QGroundControl
-    "cd ~/QGroundControl && ./QGroundControl.AppImage"
+    "cd ~/QGroundControl && ./QGroundControl.AppImage",
+
+    #Start Gazebo bridge to forward a topic to read rotors speed
+    "sleep 10 && ros2 run ros_gz_bridge parameter_bridge /x500_0/command/motor_speed@actuator_msgs/msg/Actuators[gz.msgs.Actuators"
 ]
+
+# Default terminal command on Ubuntu
+terminal_cmd = 'x-terminal-emulator'
 
 # Loop through each command in the list
 for command in commands:
-    # Each command is run in a new tab of the gnome-terminal
-    subprocess.run(["gnome-terminal", "--tab", "--", "bash", "-c", command + "; exec bash"])
-    
+    # Build the full command
+    full_cmd = [terminal_cmd, '-e', f"bash -c '{command}; exec bash'"]
+    # Run the command
+    subprocess.Popen(full_cmd)
     # Pause between each command
     time.sleep(1)
