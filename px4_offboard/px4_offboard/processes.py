2f4406deff9c2537f3147d23f9466481a48ee30f
Alessandro Orciari <92159043+AleOrcia@users.noreply.github.com>
2024-10-15 19:55:49 +0200

    Update processes.py
    refactoring

--- px4_offboard/px4_offboard/processes.py
+++ px4_offboard/px4_offboard/processes.py
@@ -18,13 +18,10 @@ commands = [
     "sleep 10 && ros2 run ros_gz_bridge parameter_bridge /x500_0/command/motor_speed@actuator_msgs/msg/Actuators[gz.msgs.Actuators"
 ]
 
-# Default terminal command on Ubuntu
-terminal_cmd = 'gnome-terminal'
-
+# Loop through each command in the list
 for command in commands:
-    # Build the full command
-    full_cmd = [terminal_cmd, '-e', f"bash -c '{command}; exec bash'"]
-    # Run the command
-    subprocess.Popen(full_cmd)
+    # Each command is run in a new tab of the gnome-terminal
+    subprocess.run(["gnome-terminal", "--tab", "--", "bash", "-c", command + "; exec bash"])
+    
     # Pause between each command
     time.sleep(1)
