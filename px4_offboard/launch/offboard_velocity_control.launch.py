a008485b304c796130b7ff6dcdf5b64fdf7c56b9
Alessandro Orciari <alessandro.orciari@studio.unibo.it>
2024-10-20 21:50:27 +0200

    Update
    Updated deprecated QoS policy
    Added functionality to connect to Unity through WebSocket
    Removed QGroundControl

--- px4_offboard/launch/offboard_velocity_control.launch.py
+++ px4_offboard/launch/offboard_velocity_control.launch.py
@@ -1,83 +1,40 @@
-#!/usr/bin/env python
-############################################################################
-#
-#   Copyright (C) 2022 PX4 Development Team. All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-# 1. Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in
-#    the documentation and/or other materials provided with the
-#    distribution.
-# 3. Neither the name PX4 nor the names of its contributors may be
-#    used to endorse or promote products derived from this software
-#    without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
-# OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
-# AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-############################################################################
-
-__author__ = "Braden Wagstaff"
-__contact__ = "braden@arkelectron.com"
-
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess
 from launch_ros.actions import Node
-from ament_index_python.packages import get_package_share_directory
-import os
+from launch.actions import ExecuteProcess
 
 
 def generate_launch_description():
-    package_dir = get_package_share_directory('px4_offboard')
-    # bash_script_path = os.path.join(package_dir, 'scripts', 'TerminatorScript.sh')
-    return LaunchDescription([
-        # ExecuteProcess(cmd=['bash', bash_script_path], output='screen'),
-        Node(
-            package='px4_offboard',
-            namespace='px4_offboard',
-            executable='visualizer',
-            name='visualizer'
-        ),
-        Node(
-            package='px4_offboard',
-            namespace='px4_offboard',
-            executable='processes',
-            name='processes',
-            prefix='gnome-terminal --'
-        ),
-        Node(
-            package='px4_offboard',
-            namespace='px4_offboard',
-            executable='control',
-            name='control',
-            prefix='gnome-terminal --',
-        ),
-        Node(
-            package='px4_offboard',
-            namespace='px4_offboard',
-            executable='velocity_control',
-            name='velocity'
-        ),
-        Node(
-            package='rviz2',
-            namespace='',
-            executable='rviz2',
-            name='rviz2',
-            arguments=['-d', [os.path.join(package_dir, 'visualize.rviz')]]
-        )
-    ])
+    nodes = [
+            # PX4 <-> ROS2 bridge
+            ExecuteProcess(name="MicroXRCEAgent", cmd=["MicroXRCEAgent", "udp4", "-p", "8888"], shell=True),
+
+            #Forward topic di Gazebo su topic ROS2 per velocità motori
+            Node(package="ros_gz_bridge", executable="parameter_bridge", name="BridgeGZTopicToRos", arguments=[
+                    '/x500_0/command/motor_speed@actuator_msgs/msg/Actuators[gz.msgs.Actuators'
+            ]),
+
+            # PX4 simulator + Gazebo
+            ExecuteProcess(name="PX4", cmd=["HEADLESS=1" ,"make", "-C", "/home/user_thesis/PX4-Autopilot", "px4_sitl", "gz_x500"], prefix="gnome-terminal --", shell=True),
+
+            # WebSocket server su porta 9090
+            Node(package="rosbridge_server", executable="rosbridge_websocket", name="rosbridge"),
+
+            # Terminale di controllo
+            Node(package="px4_offboard", executable="control", prefix="gnome-terminal --", name="term_controller"),
+            
+            # Bridge tra /fcu e il terminale di controllo
+            Node(package="px4_offboard", executable="velocity_control", name="velocity_controller"),
+
+            # Bridge tra /fcu e /px4_visualizer. Traduce i messaggi da PX4 a ROS2
+            Node(package="px4_offboard", executable="visualizer"),
+
+            # RVIZ2 Visualizer
+            Node(package="rviz2", executable="rviz2", arguments=[
+                 '-d', ['/home/user_thesis/ros2_px4_quadcopter_controller_ws/src/ROS2_PX4_Quadcopter_Controller/px4_offboard/resource/visualize.rviz']
+            ]),
+
+            # Grafo di nodi e topic
+            ExecuteProcess(cmd=["rqt_graph"]),
+    ]
+
+    return LaunchDescription(nodes)
