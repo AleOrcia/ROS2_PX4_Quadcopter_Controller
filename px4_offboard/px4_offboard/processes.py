55e10dfb831779e5bd333e4a92fe9b7b07751c12
Alessandro Orciari <92159043+AleOrcia@users.noreply.github.com>
2024-10-20 18:35:16 +0200

    Update processes.py

--- px4_offboard/px4_offboard/processes.py
+++ px4_offboard/px4_offboard/processes.py
@@ -9,13 +9,10 @@ commands = [
     "MicroXRCEAgent udp4 -p 8888",
 
     # Run the PX4 SITL simulation
-    "cd ~/PX4-Autopilot && HEADLESS=1 make px4_sitl gz_x500",
+    "cd ~/PX4-Autopilot && make px4_sitl gz_x500",
 
     # Run QGroundControl
     "cd ~/QGroundControl && ./QGroundControl.AppImage",
-
-    #Start Gazebo bridge to forward a topic to read rotors speed
-    "sleep 10 && ros2 run ros_gz_bridge parameter_bridge /x500_0/command/motor_speed@actuator_msgs/msg/Actuators[gz.msgs.Actuators"
 ]
 
 # Loop through each command in the list
